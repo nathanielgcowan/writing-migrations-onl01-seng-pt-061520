24b2acd01507fe14fe69cb58f9c32251ece6d573
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-11 20:49:16 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    add_column :students, :grade, :integer
+    change_column :students, :grade, :integer
     add_column :students, :birthdate, :string
   end
 end
